1bed6d42fb632aec11f5758d1ff6fd77546939c5
Daniel Turing <mail@danielturing.com>
2019-04-03 17:22:34 +0200

    update stencil, misc

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,8 +8,9 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'www',
+      dir: 'wdt',
       // uncomment the following line to disable service workers in production
-      // serviceWorker: null
+      //serviceWorker: null
     }
   ],
   devServer: {
