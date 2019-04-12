3606d52b0ccb0c3b9aaaf92ac2b54d8490d24436
Daniel Turing <mail@danielturing.com>
2019-04-12 18:14:15 +0200

    add a tasks list, and StandAtAngle/MoveTo tasks

--- src/components/w-tasks/w-tasks.tsx
+++ src/components/w-tasks/w-tasks.tsx
@@ -1,7 +1,126 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, Method } from '@stencil/core';
 import { RouterHistory } from '@stencil/router';
 import { WAnalysis } from '../../helpers/w-analysis';
 import { CV as cv } from '../../helpers/cv';
+
+class Task {
+
+  check(_trackBox) :boolean {
+    return true;
+  }
+
+  draw(_ctx,_w,_h) {
+  }
+
+  toString() {
+    return "<base task>";
+  }
+}
+
+class Settle extends Task {
+  framesToGo: number = 30;
+
+  check(_trackBox) :boolean {
+    return (this.framesToGo-- <= 0);
+  }
+}
+
+class StandAtAngle extends Task {
+  targetAngle: number;
+  lastAngle: number;
+  required: number = 5;
+
+  constructor(targetAngle) {
+    super();
+    this.targetAngle = targetAngle;
+  }
+
+  check(trackBox) :boolean {
+    this.lastAngle = trackBox.angle;
+    if (Math.abs( this.targetAngle - this.lastAngle ) < 2) {
+      this.required--;
+    } else {
+      this.required = 5;
+    }
+
+    return this.required <= 0;
+  }
+
+  draw(ctx,w,h) {
+
+      let rotation = ((90-this.targetAngle)/180)*Math.PI;
+      ctx.save();
+        ctx.fillStyle = "red";
+        ctx.translate(w/2,h/2);
+        ctx.rotate(rotation);
+        ctx.fillRect(-2, -1000, 4, 2000);
+      ctx.restore();
+
+      ctx.translate(100,50);
+      ctx.fillStyle = "white";
+      ctx.textAlign = "left";
+
+      ctx.font = "bold 26px Barlow";
+      ctx.fillText("Stand at an Angle of "+Math.round(this.targetAngle)+"°", 0, 10);
+
+      ctx.font = "64px Barlow";
+      ctx.fillText(""+Math.round(this.lastAngle)+"°", 0, 80);
+  }
+
+  toString() {
+    return "Stand at an Angle of "+this.targetAngle+"°";
+  }
+}
+
+class MoveTo extends Task {
+  targetX: number;
+  targetY: number;
+  lastX: number;
+  lastY: number;
+  required: number = 25;
+
+  constructor(targetX, targetY) {
+    super();
+    this.targetX = targetX;
+    this.targetY = targetY;
+  }
+
+  check(trackBox) :boolean {
+    this.lastX = trackBox.x;
+    this.lastY = trackBox.y;
+    if ( Math.abs( this.targetX - this.lastX ) < 0.01
+      && Math.abs( this.targetY - this.lastY ) < 0.01) {
+      this.required--;
+    } else {
+      this.required = 25;
+    }
+
+    return this.required <= 0;
+  }
+
+  draw(ctx,w,h) {
+      ctx.save();
+        ctx.fillStyle = "red";
+        ctx.translate(w*this.targetX,h*this.targetY);
+        ctx.fillRect(-2, -100, 4, 200);
+        ctx.fillRect(-100, -2, 200, 4);
+      ctx.restore();
+
+      ctx.translate(100,50);
+      ctx.fillStyle = "white";
+      ctx.textAlign = "left";
+
+      ctx.font = "bold 26px Barlow";
+      ctx.fillText("Move to "+(Math.round(this.targetX*100)/100)+"/"+(Math.round(this.targetY*100)/100), 0, 10);
+
+      ctx.font = "48px Barlow";
+      ctx.fillText(""+(Math.round(this.lastX*100)/100)+"/"+(Math.round(this.lastY*100)/100), 0, 60);
+  }
+
+  toString() {
+    return "Move to "+(Math.round(this.targetX*100)/100)+"/"+(Math.round(this.targetY*100)/100);
+  }
+}
 
 @Component({
   tag: 'w-tasks',
@@ -24,7 +143,21 @@ export class WCapture {
   private trackBox:any;
   private lastangle = -10000;
 
-  componentDidLoad () {
+  private audioElement!: HTMLAudioElement;
+
+  private currentTask:Task = new Settle();
+  private taskIndex = 0;
+  private tasks:Array<Task> = [
+    new MoveTo(0.5, 0.8),
+    new StandAtAngle(90),
+    new StandAtAngle(15),
+    new StandAtAngle(30),
+    new StandAtAngle(45),
+    new StandAtAngle(60),
+    new StandAtAngle(75),
+  ];
+
+  componentDidLoad() {
     if (this.analysis) {
       console.log("Analysis exists. clear first");
       this.analysis.unload();
@@ -39,16 +172,9 @@ export class WCapture {
       },
       (src, width, height, t) => {
 
-        return this.analyze(src, width, height, t);
+        var r = this.analyze(src, width, height, t);
 
-      },
-      (ctx,w,h) => {
-        ctx.clearRect(0,0,w,h);
-        ctx.save();
-
-        let cs = this.trackBox;
-        if (cs) {
-            let angle = 90-cs.angle;
+            let angle = 90-this.trackBox.angle;
 
             let sh=90;
             if( this.lastangle != -10000 ) {
@@ -59,6 +185,30 @@ export class WCapture {
             while( angle < 0 ) angle += 360;
             while( angle > 360 ) angle -= 360;
             this.lastangle = angle;
+
+            this.trackBox.angle = angle;
+            this.trackBox.x = this.trackBox.center.x/width;
+            this.trackBox.y = this.trackBox.center.y/height;
+
+        if (this.currentTask) {
+          if (this.currentTask.check(this.trackBox)) {
+            console.log("Task fulfilled: ", this.currentTask);
+            this.playSound();
+            this.currentTask = this.tasks[this.taskIndex];
+            this.taskIndex = (this.taskIndex+1)%this.tasks.length;
+          }
+        }
+
+        return r;
+
+      },
+      (ctx,w,h) => {
+        ctx.clearRect(0,0,w,h);
+        ctx.save();
+
+        let cs = this.trackBox;
+        if (cs) {
+            let angle = cs.angle;
 
             let rotation = ((90-angle)/180)*Math.PI;
 
@@ -76,13 +226,17 @@ export class WCapture {
             ctx.fillRect(-5, -cs.size.height / 2 - 10, 10, 10);
 
             ctx.restore();
-
+/*
             ctx.translate(100,50);
             ctx.font = "26px Barlow";
             ctx.fillStyle = "white";
             ctx.textAlign = "right";
             ctx.fillText(""+Math.round(angle)+"°", 0, 10);
+*/
+        }
 
+        if (this.currentTask) {
+          this.currentTask.draw(ctx, w, h);
         }
 
         ctx.restore();
@@ -141,9 +295,13 @@ export class WCapture {
     if (this.background) this.background.delete();
   }
 
+  @Method() playSound() {
+    this.audioElement.play();
+  }
 
   render() {
     return <div>
+        <audio src="/assets/sound/pluck.ogg" preload="auto" ref={ (el) => this.audioElement = el as HTMLAudioElement }></audio>
         <w-commandpalette commands={{
           "q": { symbol:"q", description:"Quit", execute:()=>{ this.history.replace("/", {}); } },
           " ": { symbol:"␠", description:"Reset", execute:()=>{ this.background.delete(); this.background=null; } },
